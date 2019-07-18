5476f5148050b5b551bd561150144aec4bd036d3
rebekasultana <rebekasultana5454@gmail.com>
2019-07-18 10:51:07 +0600

    second

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class Basic {
+
+	public static void main(String[] args) {
+		 int a = 62; 
+	        int b = 18; 
+	     int  sum = a + b;
+	        System.out.println("The sum is =" + sum); 
+	      	
+	}
+
+}
